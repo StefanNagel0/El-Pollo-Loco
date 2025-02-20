6c95b8fe86c75d22b997114d1e037855aae39f6e
StefanNagel0 <stefannagel14@hotmail.de>
2025-02-20 20:34:00 +0100

    Endboss added

--- classes/character.class.js
+++ classes/character.class.js
@@ -38,10 +38,7 @@ class Character extends MovableObject {
         setInterval(() => {
             if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT){
                 //WALK Animation
-            let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6
-            let path = this.IMAGES_WALKING[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;                
+                this.playAnimation(this.IMAGES_WALKING);
             }
         }, 50);
     }
--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -22,6 +22,13 @@ class MovableObject{
         })
     }
 
+    playAnimation(images){
+        let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6
+        let path = images[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;    
+    }
+
     moveRight(){
         console.log('Moving right');
     }

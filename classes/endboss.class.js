1309ef05b99e2327fb3c67571d64583d6f60fa23
StefanNagel0 <stefannagel14@hotmail.de>
2025-04-06 08:33:20 +0200

    background Images fixed

--- classes/endboss.class.js
+++ classes/endboss.class.js
@@ -18,7 +18,7 @@ class Endboss extends MovableObject {
     constructor(){
         super().loadImage(this.IMAGES_WALKING[0]);
         this.loadImages(this.IMAGES_WALKING);
-        this.x = 4000;
+        this.x = 5000;
         this.animate();
 
 

9ae548c66b5d050a0a2b05c40493c6b86a1c4b73
StefanNagel0 <stefannagel14@hotmail.de>
2025-04-06 10:17:23 +0200

    added more clouds

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -1,19 +1,17 @@
-class Cloud extends MovableObject{
+class Cloud extends MovableObject {
     y = 10;
     height = 250;
     width = 500;
 
-    constructor(){
-        super().loadImage('../assets/img/5_background/layers/4_clouds/1.png');
-
-        this.x =  Math.random() * 500;
+    constructor(imagePath, x) {
+        super().loadImage(imagePath || '../assets/img/5_background/layers/4_clouds/1.png');
+        this.x = x || Math.random() * 500;
         this.animate();
-        
     }
 
-    animate(){
-        setInterval(() =>{
+    animate() {
+        setInterval(() => {
             this.moveLeft();     
-        },1000 / 60); 
+        }, 1000 / 60); 
     }
 }
--- levels/level1.js
+++ levels/level1.js
@@ -9,7 +9,18 @@ const level1 = new Level(
         new Endboss(),
     ],
     [
-        new Cloud()
+        new Cloud('../assets/img/5_background/layers/4_clouds/1.png', 100),
+        new Cloud('../assets/img/5_background/layers/4_clouds/2.png', 800),
+        new Cloud('../assets/img/5_background/layers/4_clouds/1.png', 1600),
+        new Cloud('../assets/img/5_background/layers/4_clouds/2.png', 2400),
+        new Cloud('../assets/img/5_background/layers/4_clouds/1.png', 3200),
+        new Cloud('../assets/img/5_background/layers/4_clouds/2.png', 4000),
+        new Cloud('../assets/img/5_background/layers/4_clouds/1.png', 4800),
+        new Cloud('../assets/img/5_background/layers/4_clouds/2.png', 5600),
+        new Cloud('../assets/img/5_background/layers/4_clouds/1.png', 6400),
+        new Cloud('../assets/img/5_background/layers/4_clouds/2.png', 7200),
+        new Cloud('../assets/img/5_background/layers/4_clouds/1.png', 8000),
+        new Cloud('../assets/img/5_background/layers/4_clouds/2.png', 8800),
     ],
     [
         new BackgroundObject('../assets/img/5_background/layers/air.png', -719,),

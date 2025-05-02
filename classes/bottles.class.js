b753db45fe879f4130aecf60af1b4c727e3486fc
StefanNagel0 <stefannagel14@hotmail.de>
2025-05-02 22:07:09 +0200

    JSDOC bottles.class.js

--- classes/bottles.class.js
+++ classes/bottles.class.js
@@ -1,9 +1,19 @@
+/**
+ * Represents a bottle collectible in the game.
+ * Bottles can be collected by the player and used as throwable objects.
+ * @class
+ * @extends MovableObject
+ */
 class Bottles extends MovableObject {
     height = 100;
     width = 100;
     static IMAGE_PATH = '../assets/img/6_salsa_bottle/1_salsa_bottle_on_ground.png';
-    static MAX_POSITION_ATTEMPTS = 100; // Maximale Versuche zur Positionsfindung
+    static MAX_POSITION_ATTEMPTS = 100;
 
+    /**
+     * Creates a new bottle with a random position.
+     * The bottle is placed at a valid x-coordinate and fixed y-coordinate.
+     */
     constructor() {
         super();
         this.loadImage(Bottles.IMAGE_PATH);
@@ -12,6 +22,11 @@ class Bottles extends MovableObject {
         MovableObject.placedObjects.push(this.x);
     }
 
+    /**
+     * Finds a valid x-position for the bottle that maintains minimum distance from other objects.
+     * Uses an adaptive algorithm that reduces distance requirements after multiple failed attempts.
+     * @returns {number} A valid x-coordinate
+     */
     getValidXPosition() {
         let x;
         let isTooClose;
@@ -27,10 +42,23 @@ class Bottles extends MovableObject {
         return x;
     }
 
+    /**
+     * Checks if a position is too close to existing objects.
+     * @param {number} x - The x-coordinate to check
+     * @param {number} minDistance - Minimum required distance between objects
+     * @returns {boolean} True if position is too close to existing objects
+     */
     isPositionTooClose(x, minDistance) {
         return MovableObject.placedObjects.some(existingX => Math.abs(existingX - x) < minDistance);
     }
 
+    /**
+     * Generates a specified number of bottle objects with optimal spacing.
+     * Divides the level into segments and places bottles throughout.
+     * @param {number} count - Number of bottles to generate
+     * @returns {Array<Bottles>} Array of generated bottles
+     * @static
+     */
     static generateBottles(count) {
         let bottles = [];
         let bottlePositions = [];
@@ -45,6 +73,14 @@ class Bottles extends MovableObject {
         return bottles;
     }
 
+    /**
+     * Calculates an optimal position for a bottle within a segment.
+     * @param {number} index - The index of the segment
+     * @param {number} segmentSize - The size of each segment
+     * @param {Array<number>} existingPositions - Positions of already placed bottles
+     * @returns {number} The calculated x-coordinate
+     * @static
+     */
     static calculateBottlePosition(index, segmentSize, existingPositions) {
         const minX = 300 + index * segmentSize;
         const maxX = minX + segmentSize - 100;
@@ -57,6 +93,13 @@ class Bottles extends MovableObject {
         return x;
     }
 
+    /**
+     * Checks if a position is too close to any existing bottle positions.
+     * @param {number} position - The position to check
+     * @param {Array<number>} existingPositions - Positions of already placed bottles
+     * @returns {boolean} True if the position is too close to existing bottles
+     * @static
+     */
     static isPositionCloseToOthers(position, existingPositions) {
         const minDistance = MovableObject.minDistanceObjects / 2;
         return existingPositions.some(pos => Math.abs(pos - position) < minDistance);

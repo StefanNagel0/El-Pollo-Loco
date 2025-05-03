0924e5907e2289a3270e5f9897add3099a8fa281
StefanNagel0 <stefannagel14@hotmail.de>
2025-05-03 11:59:42 +0200

    JSDOC keyboard.class.js

--- classes/keyboard.class.js
+++ classes/keyboard.class.js
@@ -1,3 +1,8 @@
+/**
+ * Handles keyboard input state for the game controls.
+ * Tracks the state of various keys needed for player movement and actions.
+ * @class
+ */
 class Keyboard{
     LEFT = false;
     RIGHT = false;

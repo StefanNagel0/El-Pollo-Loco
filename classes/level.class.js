92128872c43c5b9a9078d6d9fe61578baff49afa
StefanNagel0 <stefannagel14@hotmail.de>
2025-05-03 12:01:03 +0200

    JSDOC level.class.js

--- classes/level.class.js
+++ classes/level.class.js
@@ -1,3 +1,8 @@
+/**
+ * Represents a game level with all its components.
+ * Contains enemies, clouds, background objects, collectible items and boundaries.
+ * @class
+ */
 class Level{
     enemies;
     clouds;
@@ -6,6 +11,14 @@ class Level{
     backgroundObjects;
     level_end_x = 6500; 
 
+        /**
+     * Creates a new level with the specified components.
+     * @param {Array<MovableObject>} enemies - The enemies in this level
+     * @param {Array<Cloud>} clouds - The clouds in this level
+     * @param {Array<BackgroundObject>} backgroundObjects - The background objects in this level
+     * @param {Array<Coin>} coins - The coins in this level
+     * @param {Array<Bottles>} bottles - The bottles in this level
+     */
     constructor(enemies, clouds, backgroundObjects, coins, bottles){
         this.enemies = enemies;
         this.clouds = clouds;
